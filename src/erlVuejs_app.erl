bf180b4fd24bd5a669c41a50efc2c8bf9c47725e
Narongchai Thongkongkeaw <dotnatcore@gmail.com>
2017-03-20 15:37:41 +0700

    Add test http handler

--- src/erlVuejs_app.erl
+++ src/erlVuejs_app.erl
@@ -15,6 +15,15 @@
 %%====================================================================
 
 start(_StartType, _StartArgs) ->
+    Dispatch = cowboy_router:compile([
+    {'_', [
+        {"/", test_handler, []}
+    ]}
+	]),
+	{ok, _} = cowboy:start_clear(http, 100, [{port, 8080}], #{
+		env => #{dispatch => Dispatch}
+	}),
+    io:format("Start App Port 8080 ~n"),
     erlVuejs_sup:start_link().
 
 %%--------------------------------------------------------------------
